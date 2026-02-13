0140b3a954efad9408abea2b1345bb8c0c63335a
kentman0723-beep <kentman0723@gmail.com>
2026-02-13 17:55:28 +0900

    Fix: add basename to BrowserRouter for GitHub Pages routing

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename="/frame.one">
             <AppProvider>
                 <App />
             </AppProvider>
